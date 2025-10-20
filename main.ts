cf2ae0986ec082c012e2399501c97519405d534c
timhealey3 <timhealey34@gmail.com>
2025-10-19 23:42:49 -0400

    initial commit

---
+++ main.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+
+const ymlContent = fs.readFileSync("Application.yml", "utf8");
+const config = yaml.load(ymlContent) as { website: { url: string } };
+const web_url = config.website.url;
+
+async function main() {
+  const url = web_url;
+  console.log("test")
+  try {
+    // Download HTML
+    const { data } = await axios.get(url);
+
+    // Load HTML into Cheerio
+    const $ = cheerio.load(data);
+
+    // Extract links
+    const links: string[] = [];
+    $("a").each((_, el) => {
+      const href = $(el).attr("href");
+      if (href && href.includes("/nhl/game/_/gameId/")) {
+        links.push(href);
+      }
+    });
+
+    const texts: string[] = [];
+    $("a").each((_, el) => {
+      const text = $(el).text().trim();
+      if (text) {
+        texts.push(text);
+      }
+    });
+
+    console.log("Links found:", links);
+    texts.forEach(element => {
+      console.log(element);
+    });
+  } catch (error) {
+    console.error("Error scraping:", error);
+  }
+}
+
+main();
