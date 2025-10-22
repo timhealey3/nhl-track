97e7a37e92ca0be0fbe61f59e06d6e9b7c3186c6
timhealey3 <timhealey34@gmail.com>
2025-10-21 22:35:25 -0400

    create game selector

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import * as yaml from "js-yaml";
 
+
   // Read the YAML file using Deno
 const ymlContent = await Deno.readTextFile("../Application.yml");
   // Parse YAML
@@ -9,8 +10,10 @@ const config = yaml.load(ymlContent) as { website: { url: string } };
 const web_url = config.website.url;
 
 async function main() {
+  const games: string[] = [];
   const url = web_url;
-  console.log("test")
+  const currentDate: Date = new Date();
+  console.log("NHL GAMES TODAY: " + currentDate.getMonth() + " - " + currentDate.getDate() + " - " + currentDate.getFullYear());
   try {
     // Download HTML
     const { data } = await axios.get(url);
@@ -18,20 +21,45 @@ async function main() {
     // Load HTML into Cheerio
     const $ = cheerio.load(data);
 
-    // Extract specific data
-    const jsonLdScript = $('script[type="application/ld+json"]').html();
-
-    if (jsonLdScript) {
+    let idk = $('script[data-name="liveScoringScript"]').html();
+    if (!idk) return;
+    const idkMatch = idk.match(/return\s+(\{[\s\S]*?\});/);
+    const json = JSON.parse(idkMatch!![1]);
+    json.gameAbbr.split('|').forEach(element => {
+      games.push(element);
+    });
+    
+    console.log(games);
+    let gameCounter: number = 1;
+    $('script[type="application/ld+json"]').each((_, el) => {
+      let content = $(el).html();
+  
+      if (!content) return;
+  
+      // remove newlines
+      content = content
+        .replace(/(\r\n|\n|\r)/gm, "");
+  
+      // get all games for today
       try {
-        const jsonData = JSON.parse(jsonLdScript);
-        console.log("Event Name:", jsonData.name);
-      } catch {
+        const json = JSON.parse(content);
+        if (json["@type"] === "SportsEvent") {
+          console.log(`${gameCounter}: `, json.name);
+          gameCounter++;
+        }
+      } catch (err) {
+        console.error("JSON parse failed:", err.message);
       }
-    }
+    });
+    let name: string = " ";
 
-    console.log("Links found:", jsonLdScript);
-  } catch (error) {
-    console.error("Error scraping:", error);
+    while (name && isNaN(parseInt(name))) {
+      name = prompt("Select which game you would like to watch >");
+    }
+    console.log(`selected: ${games[parseInt(name) - 1]}`);
+  }
+  catch {
+    console.log("Webscrape failed");
   }
 }
 
